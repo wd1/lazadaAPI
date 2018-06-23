e87bdadbf72c548f7b6c09250a3ec5af321ce1eb
Esteban <esteban.bouza@gmail.com>
2018-06-23 20:19:10 +0700

    Improved order retrieval, version bump

--- lib/lazada/api/order.rb
+++ lib/lazada/api/order.rb
@@ -3,44 +3,48 @@ module Lazada
     module Order
       def get_orders(options = {})
         params = {}
-        params['Status'] = options[:status] if options[:status].present?
-        params['CreatedAfter'] = options[:created_after].iso8601 if options[:created_after].present?
+        params['status'] = options[:status] if options[:status].present?
+        params['created_after'] = options[:created_after].iso8601 if options[:created_after].present?
+        params['limit'] = options[:limit] || 100
+        params['offset'] = options[:offset] || 0
+        params['sort_by'] = options[:sort_by] || 'created_at'
+        params['sort_direction'] = options[:sort_direction] || "DESC"
 
-        url = request_url('GetOrders', params)
+        url = request_url('/orders/get', params)
         response = self.class.get(url)
 
         process_response_errors! response
 
-        return response['SuccessResponse']['Body']['Orders']
+        return response['data']
       end
 
       def get_order(id)
-        url = request_url('GetOrder', { 'OrderId' => id })
+        url = request_url('/order/get', { 'order_id' => id })
         response = self.class.get(url)
 
         process_response_errors! response
 
-        return response['SuccessResponse']['Body']['Orders']
+        return response['data']
       end
 
       def get_order_items(id)
-        url = request_url('GetOrderItems', { 'OrderId' => id })
+        url = request_url('/order/items/get', { 'order_id' => id })
         response = self.class.get(url)
 
         process_response_errors! response
 
-        return response['SuccessResponse']['Body']['OrderItems']
+        return response['data']
       end
 
       def get_multiple_order_items(ids_list)
         raise Lazada::LazadaError("IDs list must be an Array of integers or strings") unless ids_list.is_a?(Array)
 
-        url = request_url('GetMultipleOrderItems', { 'OrderIdList' => "[#{ids_list.join(',')}]"})
+        url = request_url('/orders/items/get', { 'order_ids' => "[#{ids_list.join(',')}]"})
         response = self.class.get(url)
 
         process_response_errors! response
 
-        return response['SuccessResponse']['Body']['Orders']
+        return response['data']
       end
     end
   end
